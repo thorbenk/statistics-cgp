12fda5444c18422744702774181856a6b8f55da8
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2014-05-12 12:11:30 +0200

    begin implementing compression statistics

--- cgp_statistics.cxx
+++ cgp_statistics.cxx
@@ -1,7 +1,11 @@
+#include <map>
+
 #include <boost/program_options.hpp>
 
 #include <vigra/accumulator.hxx>
 #include <vigra/hdf5impex.hxx>
+#include <vigra/compression.hxx>
+#include <vigra/timing.hxx>
 
 #include <cgp/GeometryReader.hxx>
 
@@ -40,6 +44,7 @@ void supervoxelStatistics(const vigra::MultiArrayView<3, uint32_t>& seg) {
 int main(int argc, char** argv) {
     namespace po = boost::program_options;
     using namespace vigra;
+    using std::cout; using std::endl; using std::flush;
     
     po::options_description desc("Allowed options");
     desc.add_options()
@@ -48,6 +53,8 @@ int main(int argc, char** argv) {
          "geometry file")
         ("seg", po::value<std::string>(),
          "seg file")
+        ("tg", po::value<std::string>(),
+         "tg file")
     ;
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -56,6 +63,7 @@ int main(int argc, char** argv) {
     std::string geomFile;
     std::string segFile;
     std::string segGroup;
+    std::string tgFile;
     if (vm.count("help")) {
         cout << desc << endl;
         return 1;
@@ -63,13 +71,16 @@ int main(int argc, char** argv) {
     if (vm.count("geom")) {
         geomFile = vm["geom"].as<std::string>();
     } 
+    if (vm.count("tg")) {
+        tgFile = vm["tg"].as<std::string>();
+    } 
     if (vm.count("seg")) {
         std::string s = vm["seg"].as<std::string>();
         auto pos = s.find_last_of("/");
         segFile = s.substr(0,pos);
         segGroup = s.substr(pos+1,s.size());
     } 
-    if (geomFile.empty() && segFile.empty()) {
+    if (geomFile.empty() && segFile.empty() && tgFile.empty()) {
         cout << "Error: Need at least one of --geom and --seg options!" << endl << endl;
         cout << desc << endl;
         return 1;
@@ -94,6 +105,44 @@ int main(int argc, char** argv) {
                                 GeometryReader::EnableThreeSetBoundedByQuery);
         for(int d=1; d<3; ++d) {
             statistic(g, d);
+        }
+    }
+    
+    if(!tgFile.empty()) {
+        std::map<std::string, CompressionMethod> cm;
+        cm["DEFAULT_COMPRESSION"] = DEFAULT_COMPRESSION;
+        cm["NO_COMPRESSION     "] = NO_COMPRESSION;
+        cm["ZLIB_NONE          "] = ZLIB_NONE;
+        cm["ZLIB_FAST          "] = ZLIB_FAST;
+        cm["ZLIB               "] = ZLIB;
+        cm["ZLIB_BEST          "] = ZLIB_BEST;
+        cm["LZ4                "] = LZ4;
+        
+        USETICTOC;
+        
+        MultiArray<3, uint32_t> tg;
+
+        for(const auto& kv : cm) {
+            const auto cname = kv.first;
+            const auto cflag = kv.second;
+            cout << "compressing with " << cname << " ... " << flush;
+            
+            ArrayVector<char> dest;
+            double avg = 0.0;
+        
+            //void compress(char const * source, std::size_t size, ArrayVector<char> & dest, CompressionMethod method);
+            HDF5File f(tgFile, HDF5File::OpenReadOnly);
+            f.cd("blocks");
+            auto ls = f.ls();
+            for(const auto& x : ls) {
+                f.cd(x);
+                f.readAndResize("topological-grid", tg);
+                TIC;
+                compress(reinterpret_cast<const char*>(tg.data()), tg.size()*sizeof(uint32_t), dest, cflag);
+                avg += TOCN;
+                f.cd_up();
+            }
+            cout << " (" << ls.size() << " blocks)" << " took on average " << avg << endl;
         }
     }
 }
